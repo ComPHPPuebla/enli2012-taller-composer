664e83e7ee7837ad1cd7d8ef7c96c541d352de63
Luis Montealegre <montealegreluis@gmail.com>
2016-05-21 21:31:02 -0500

    Use Dotenv library to remove the connection credentials.

--- step-2/list-books.php
+++ step-2/list-books.php
@@ -16,16 +16,34 @@ try {
     $statement = $conn->prepare('SELECT * FROM book');
     $statement->execute();
     $books = $statement->fetchAll();
-
-    //Setup the view Layer
-    $loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
-    $twig = new Twig_Environment($loader, [
-        'cache' => __DIR__ . '/var/cache',
-        'strict_variables' => true,
-        'debug' => true,
-    ]);
-    echo $twig->render('list.html.twig', ['books' => $books]);
 } catch (PDOException $e) {
     error_log("PDO Exception: \n{$e}\n");
     http_response_code(500);
+    die;
 }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Catálogo de libros</title>
+    <meta charset="utf8">
+</head>
+<body>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Título</th>
+    </tr>
+    <?php foreach ($books as $book) : ?>
+    <tr>
+        <td>
+            <a href="show-book-detail.php?id=<?= $book['book_id'] ?>">
+                Detalles
+            </a>
+        </td>
+        <td><?= $book['title'] ?></td>
+    <tr>
+    <?php endforeach ?>
+</table>
+</body>
+</html>
--- step-2/show-book-detail.php
+++ step-2/show-book-detail.php
@@ -13,7 +13,7 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
     ]);
-    $bookId = (int) $_GET['id'];
+    $bookId = (int)$_GET['id'];
     $sql = <<<SELECT
     SELECT
         b.title,
@@ -25,16 +25,27 @@ SELECT;
     $statement = $connection->prepare($sql);
     $statement->execute([$bookId]);
     $book = $statement->fetch();
-
-    //Setup the view Layer
-    $loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
-    $twig = new Twig_Environment($loader, [
-        'cache' => __DIR__ . '/var/cache',
-        'strict_variables' => true,
-        'debug' => true,
-    ]);
-    echo $twig->render('show.html.twig', ['book' => $book]);
 } catch (PDOException $e) {
     error_log("PDO Exception: \n{$e}\n");
     http_response_code(500);
+    die;
 }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Catálogo de libros</title>
+    <meta charset="utf8">
+</head>
+<body>
+<dl>
+    <dt>Título</dt>
+    <dd><?= $book['title'] ?></dd>
+    <dt>Autor</dt>
+    <dd><?= $book['author'] ?></dd>
+</dl>
+<p>
+    <a href="list-books.php">Volver a la lista</a>
+</p>
+</body>
+</html>

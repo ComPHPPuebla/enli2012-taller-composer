ec6006efbc283b9948bba0b83ec287aad52de78b
Luis Montealegre <montealegreluis@gmail.com>
2016-05-25 21:19:58 -0500

    Remove old code.

--- src/Dispatcher/ControllerNotFoundException.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace ComPHPPuebla\Dispatcher;
-
-class ControllerNotFoundException extends NotFoundException {}
--- src/Dispatcher/NotFoundException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace ComPHPPuebla\Dispatcher;
-
-use \Exception;
-
-class NotFoundException extends Exception {}
